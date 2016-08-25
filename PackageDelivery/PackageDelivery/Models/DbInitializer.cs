a58d1e090b6ce018b25333c706be9afc97051582
Eirik Baug <eirikbaug@hotmail.com>
2016-08-25 14:14:36 +1000

    skeleton of project with database setup and basic homepage design

---
+++ PackageDelivery/PackageDelivery/Models/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PackageDelivery.Models
+{
+    public class DbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+
+        // Add some test data, which is always smart to do.
+        protected override void Seed(ApplicationDbContext db)
+        {
+            // These two managers handle storage in the given db context for us
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+        }
+    }
+}
---
+++ PackageDelivery/PackageDelivery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PackageDelivery.Startup))]
+namespace PackageDelivery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
